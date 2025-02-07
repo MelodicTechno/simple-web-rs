b845d0c2a34587923807e135a5ee6d0e65849adf
MelodicTechno <617179025@qq.com>
2025-02-07 13:11:21 +0800

    concurrent

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,35 @@
 use std::fs;
-use std::io::prelude::*;
-use std::net::TcpListener;
-use std::net::TcpStream;
+use async_std::io::prelude::*;
+use async_std::net::TcpListener;
+use std::time::Duration;
+use async_std::task;
+use async_std::net::TcpStream;
+use futures::stream::StreamExt;
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+#[async_std::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").await.unwrap();
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        handle_connection(stream);
-    }
-    println!("Hello, world!");
+    listener
+        .incoming()
+        .for_each_concurrent(None, |tcpstream| async move {
+            let tcpstream = tcpstream.unwrap();
+            handle_connection(tcpstream).await;
+        })
+        .await;
 }
 
-fn handle_connection(mut stream: TcpStream) {
+async fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
+    stream.read(&mut buffer).await.unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, file_name) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        task::sleep(Duration::from_secs(5)).await;
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
@@ -29,6 +38,6 @@ fn handle_connection(mut stream: TcpStream) {
     let contents = fs::read_to_string(file_name).unwrap();
 
     let response = format!("{status_line}{contents}");
-    stream.write_all(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    stream.write_all(response.as_bytes()).await.unwrap();
+    stream.flush().await.unwrap();
 }
